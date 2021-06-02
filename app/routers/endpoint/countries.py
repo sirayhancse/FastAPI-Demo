1d6f0155b97dc8a0286d3dead6e4dd434346f658
rayhan <sirayhancse@gmail.com>
2021-06-03 02:40:02 +0600

    initiate db dependency for calling APIs

--- app/routers/endpoint/countries.py
+++ app/routers/endpoint/countries.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
+from sqlalchemy.orm import Session
+
+from ...utils import get_db
 
 
 app = FastAPI()
@@ -10,6 +13,8 @@ router = InferringRouter()
 
 @cbv(router)
 class Countries():
+    db: Session = Depends(get_db)
+
     @router.get("/")
     def get_all_countries(self):
         return {"name": "Bangladesh"}
