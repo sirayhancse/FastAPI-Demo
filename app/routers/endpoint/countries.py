270e1d9b45920fb35225d31e6fe8695e7e316c21
rayhan <sirayhancse@gmail.com>
2021-06-03 23:38:11 +0600

    create get_countries router endpoint with params and response model

--- app/routers/endpoint/countries.py
+++ app/routers/endpoint/countries.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Depends
+from re import L
+from fastapi import FastAPI, Depends, Query
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 from sqlalchemy.orm import Session
+from typing import Optional, List
 
 from ...utils import get_db
 from app import schemas
@@ -10,7 +12,6 @@ from app.services.db.countries import Countries as CountriesService
 
 app = FastAPI()
 router = InferringRouter()
-
 
 
 @cbv(router)
@@ -23,8 +24,20 @@ class Countries():
 
         return service_countries.create_country(db=self.db, country=country)
 
-    @router.get("/")
-    def get_all_countries(self):
-        return {"name": "Bangladesh"}
+    @router.get("/", response_model=List[schemas.Country])
+    def get_all_countries(self, country_name: Optional[str] = Query("", alias="country-name"),
+                          country_code: Optional[str] = Query(
+                              "", alias="country-code"),
+                          skip: Optional[str] = Query(0, alias="skip"),
+                          limit: Optional[str] = Query(10, alias="limit")):
+        service_countries = CountriesService()
+
+        return service_countries.get_countries(db=self.db,
+                                               country_name=country_name,
+                                               country_code=country_code,
+                                               skip=skip,
+                                               limit=limit
+                                               )
+
 
 app.include_router(router)
