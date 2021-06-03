20a57bed545d3e15308e102236d7ffb62d68126e
rayhan <sirayhancse@gmail.com>
2021-06-03 23:36:40 +0600

    add get_countries method with required params

--- app/crud/db/countries.py
+++ app/crud/db/countries.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from sqlalchemy.orm import Session
+from sqlalchemy import and_, or_
 
 from app import models, schemas
 
@@ -26,11 +28,27 @@ class Countries():
 
             for address in state.addresses:
                 address_obj = models.Address(
-                name=address.name,
-                house_number=address.house_number,
-                road_number=address.road_number, 
-                state_id=state_obj.id
-            )
+                    name=address.name,
+                    house_number=address.house_number,
+                    road_number=address.road_number,
+                    state_id=state_obj.id
+                )
             self.db.add(address_obj)
 
         return country_obj
+
+    def get_countries(self, country_name, country_code, skip, limit):
+
+        if country_name and country_code:
+            return self.db.query(models.Country).filter(
+                and_(models.Country.name.ilike(str(country_name)),
+                     models.Country.code.ilike(str(country_code))
+                     )).offset(skip).limit(limit).all()
+
+        elif country_name or country_code:
+            return self.db.query(models.Country).filter(
+                or_(models.Country.name.ilike(str(country_name)),
+                    models.Country.code.ilike(str(country_code))
+                    )).offset(skip).limit(limit).all()
+        else:
+            return self.db.query(models.Country).offset(skip).limit(limit).all()
