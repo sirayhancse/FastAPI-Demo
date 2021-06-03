9f1e522377d335af5ca8cf83161dddf054b71b45
rayhan <sirayhancse@gmail.com>
2021-06-03 23:37:35 +0600

    add get_countries service method

--- app/services/db/countries.py
+++ app/services/db/countries.py
@@ -1,4 +1,6 @@
 from fastapi import HTTPException, status
+from typing import Optional
+
 from ... import schemas
 from app.crud.db.countries import Countries as CountriesCrud
 
@@ -18,5 +20,18 @@ class Countries():
             raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
                                 detail="Country created failed")
 
+    def get_countries(self, db, country_name, country_code, skip, limit):
+        db_countries = CountriesCrud(db=db)
 
-        
+        country_list = db_countries.get_countries(
+            country_name=country_name,
+            country_code=country_code,
+            skip=skip,
+            limit=limit
+        )
+
+        if country_list:
+            return country_list
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="No country found")
