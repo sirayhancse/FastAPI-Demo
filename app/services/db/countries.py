174509df5de3b45904436ead0ec6094d8384ad4b
rayhan <sirayhancse@gmail.com>
2021-06-03 22:06:54 +0600

    initiate DB crud into service layer and call create contact method with required params

--- app/services/db/countries.py
+++ app/services/db/countries.py
@@ -1,8 +1,22 @@
+from fastapi import HTTPException, status
 from ... import schemas
+from app.crud.db.countries import Countries as CountriesCrud
 
 
 class Countries():
 
     def create_country(self, db, country: schemas.CreateCountry):
-        return country
+        db_countries = CountriesCrud(db=db)
+
+        country_obj = db_countries.create_country(country=country)
+
+        if country_obj:
+            db.commit()
+            raise HTTPException(status_code=status.HTTP_201_CREATED,
+                                detail="Country created successfully")
+        else:
+            raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                                detail="Country created failed")
+
+
         
