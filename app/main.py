c13b1a805f32395d4df1df178cf4dea626fc952b
rayhan <sirayhancse@gmail.com>
2021-06-07 01:02:01 +0600

    initiate and call auth and users endpoint from main handeler

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
-#framework packages
+# framework packages
 from fastapi import FastAPI
 
-#local packages
+# local packages
 from app import routers, database, models
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
-app.include_router(routers.endpoint.countries.router, prefix="/api/v1/countries", tags=["countries"])
+app.include_router(routers.endpoint.auth.router,
+                   prefix="/api/v1/auth", tags=["auth"])
+app.include_router(routers.endpoint.users.router,
+                   prefix="/api/v1/users", tags=["users"])
+app.include_router(routers.endpoint.countries.router,
+                   prefix="/api/v1/countries", tags=["countries"])
