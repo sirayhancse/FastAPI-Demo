cdf6aa299302dd0f41ec958a47c117f1e1fc0fff
rayhan <sirayhancse@gmail.com>
2021-06-07 03:24:56 +0600

    add user registration and login best and wrost test cases modify existing test cases and add auth header

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 import json
 from fastapi.testclient import TestClient
+import pytest
 
 from app.main import app
 
@@ -13,10 +14,64 @@ country_data = test_data["country"]
 state_data = test_data["states"]
 address_data = test_data["addresses"]
 
+fake_email = "a@a.com"
+fake_password = "12345"
+
+
+def pytest_namespace():
+    return {"access_token": ""}
+
+
+def test_user_registration():
+    response = client.post("/api/v1/auth/register",
+                           headers={"X-Token": "coneofsilence"},
+                           json={
+                               "email": fake_email,
+                               "password": fake_password
+                           })
+    assert response.status_code == 201
+    assert response.json()["success"] == True
+
+
+def test_existing_user_registration():
+    response = client.post("/api/v1/auth/register",
+                           headers={"X-Token": "coneofsilence"},
+                           json={
+                               "email": fake_email,
+                               "password": fake_password
+                           })
+    assert response.status_code == 409
+    assert response.json() == {"detail": "Email already registered"}
+
+
+def test_user_login():
+    response = client.post("/api/v1/auth/login",
+                           headers={"X-Token": "coneofsilence"},
+                           data={
+                               "username": fake_email,
+                               "password": fake_password,
+                               "grant_type": "password"
+                           }
+                           )
+    if response.status_code == 200:
+        pytest.access_token = response.json()["access_token"]
+    assert response.status_code == 200
+    assert response.json()["token_type"] == "bearer"
+
+
+def test_unauthorized_access():
+    response = client.post("/api/v1/countries/",
+                           headers={
+                               "Authorization": "coneofsilence"},
+                           json=create_country_test_data)
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Not authenticated"}
+
 
 def test_create_country():
     response = client.post("/api/v1/countries/",
-                           headers={"X-Token": "coneofsilence"},
+                           headers={
+                               "Authorization": f"Bearer {pytest.access_token}"},
                            json=create_country_test_data)
     assert response.status_code == 201
     assert response.json() == {"detail": "Country created successfully"}
@@ -24,15 +79,16 @@ def test_create_country():
 
 def test_create_existing_country():
     response = client.post("/api/v1/countries/",
-                           headers={"X-Token": "coneofsilence"},
+                           headers={
+                               "Authorization": f"Bearer {pytest.access_token}"},
                            json=create_country_test_data)
-    assert response.status_code == 400
+    assert response.status_code == 409
     assert response.json() == {"detail": "Country already exists"}
 
 
 def test_get_all_countries():
     response = client.get("/api/v1/countries/",
-                          headers={"X-Token": "coneofsilence"})
+                          headers={"Authorization": f"Bearer {pytest.access_token}"})
     assert response.status_code == 200
     assert len(response.json()) == len(country_data)
     assert sorted(response.json()) == sorted(country_data)
@@ -40,7 +96,7 @@ def test_get_all_countries():
 
 def test_get_states_by_country():
     response = client.get("/api/v1/countries/1/states",
-                          headers={"X-Token": "coneofsilence"})
+                          headers={"Authorization": f"Bearer {pytest.access_token}"})
     assert response.status_code == 200
     assert len(response.json()) == len(state_data)
     assert sorted(response.json()) == sorted(state_data)
@@ -48,7 +104,7 @@ def test_get_states_by_country():
 
 def test_get_addresses_by_state():
     response = client.get("/api/v1/countries/states/1/addresses",
-                          headers={"X-Token": "coneofsilence"})
+                          headers={"Authorization": f"Bearer {pytest.access_token}"})
     assert response.status_code == 200
     assert len(response.json()) == len(address_data)
     assert sorted(response.json()) == sorted(address_data)
@@ -56,7 +112,7 @@ def test_get_addresses_by_state():
 
 def test_get_address_details():
     response = client.get("/api/v1/countries/states/address?address_name=Shantinagar",
-                          headers={"X-Token": "coneofsilence"})
+                          headers={"Authorization": f"Bearer {pytest.access_token}"})
     assert response.status_code == 200
     assert response.json() == address_details
 
@@ -64,6 +120,6 @@ def test_get_address_details():
 # try to get address details using address name as Bashundhora
 def test_get_inxistent_address_details():
     response = client.get("/api/v1/countries/states/address?address_name=Bashundhora",
-                          headers={"X-Token": "coneofsilence"})
+                          headers={"Authorization": f"Bearer {pytest.access_token}"})
     assert response.status_code == 404
     assert response.json() == {"detail": "No address found"}
